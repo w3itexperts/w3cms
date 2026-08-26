8a2271d84754a68d7f1a45bbcdcff1688ca50fcd
w3chandan <chandan.w3itexperts@gmail.com>
2026-08-26 18:47:18 +0530

    Update 2.8.2

--- Modules/SolarMitra/resources/views/business/permissions/ajax/get_users_by_role.blade.php
+++
@@ -1,5 +0,0 @@
-<option value="">{{ __('solarmitra::solarmitra.select_user') }}</option>
-@forelse($users as $user)
-	<option value="{{ $user->id }}">{{ $user->full_name }}</option>
-@empty
-@endforelse
